000bab3eb3f77ddafb4b00027246efb04654508c
Märt Sessman <m2rt.sessman@gmail.com>
2016-03-06 18:12:55 +0200

    login leht, anna hääl leht, üritasin mingit funktsionaalsust luua

--- application/views/anna_haal.php
+++ application/views/anna_haal.php
@@ -1,16 +1,27 @@
 
-    <div class="container">
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12 text-left">
-                    <h3>Anna hääl</h3>
-                    <p>Siia tuleb mingi tekst...</p>
-                    <p>Siia tuleb mingi tekst...</p>
-                    <p>Siia tuleb mingi tekst...</p>
-                    <p>Siia tuleb mingi tekst...</p>
+<div class="container">
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12 text-left">
+                <h3>Anna hääl</h3>
+                <?php if($on_logitud){
+                    echo
+                    '<div class="col-lg-5 text-left">
+                    <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Sisesta number">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Hääleta!</button>
+                    </span>
+                    </div>
+                    </div>';
+                }
+                else{
+                    echo '<p>Hääletamiseks logi sisse </p>';
+                } ?>
 
-                </div>
+
             </div>
         </div>
     </div>
-    <!-- /.container -->
+</div>
+<!-- /.container -->
--- application/views/login.php
+++ application/views/login.php
@@ -3,8 +3,18 @@
             <div class="box">
                 <div class="col-lg-12 text-center">
                     <h3> Logi sisse </h3>
-                    
-
+                    <div class="login">
+                    <p>
+                        <a href="<?php echo site_url('sait/login/Facebook'); ?>">
+                            <input type="button" value="Logi sisse Facebooki kaudu" class="btn btn-primary login" >
+                        </a>
+                    </p>
+                    <p>
+                        <a href="<?php echo site_url('sait/login/Google'); ?>">
+                        <input type="button" value="Logi sisse Google'i kontoga" class="btn btn-danger login" >
+                        </a>
+                    </p>
+                    </div>
                 </div>
             </div>
         </div>
