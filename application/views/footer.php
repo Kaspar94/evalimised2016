a71aca270cc9641362d2a6109b50e397e4357f37
Märt Sessman <m2rt.sessman@gmail.com>
2016-03-06 22:10:39 +0200

    omg...

--- application/views/footer.php
+++ application/views/footer.php
@@ -7,7 +7,7 @@
             </div>
         </div>
     </footer>
-	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.12.0.min.js"); ?>"/></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.12.0.min.js"); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.tablesorter.min.js"); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url("assets/js/custom.js"); ?>"></script>
