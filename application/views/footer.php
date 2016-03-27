724b62d630cc8f5a543188015388782e169231b4
Märt Sessman <m2rt.sessman@gmail.com>
2016-03-27 19:29:37 +0300

    bootstrap ja jquery CDN + fallback

--- application/views/footer.php
+++ application/views/footer.php
@@ -7,8 +7,19 @@
             </div>
         </div>
     </footer>
-	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.12.0.min.js"); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo base_url("assets/js/jquery-1.12.0.min.js"); ?>"><\/script>')</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script>$.fn.modal || document.write('<script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"><\/script>')</script>
+        <script>
+            (function($) {
+                $(function() {
+                    if ($('body').css('color') !== 'rgb(51, 51, 51)') {
+                        $('head').prepend('<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>">');
+                    }
+                });
+            })(window.jQuery);
+        </script>
         <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.tablesorter.min.js"); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url("assets/js/custom.js"); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url("assets/js/ajaxRequest.js"); ?>"></script>
