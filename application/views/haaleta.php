1e7d549fd6f1b7e9b3542e09066e8f62d5e8e901
Märt Sessman <m2rt.sessman@gmail.com>
2016-03-27 23:54:47 +0300

    pls work now

--- application/views/haaleta.php
+++ application/views/haaleta.php
@@ -7,21 +7,20 @@
                 <?php if($on_logitud){
                     if($haal[0]->Valik == null){
                     echo
-                    '<div class="col-lg-5 text-left">
+                    '<div class="col-lg-5 text-center">
                     <div class="input-group">
                         <input type="text" id="data" name="haaleta" class="form-control" placeholder="Sisesta number" onkeyup="showKandidaat(this.value)" />
 			<span class="input-group-btn">
                     	</span>
 			<div id="spinner"></div>
-                    
+                        <div id="kandB"></div>
                     </div>
-                    <div id="kandB"></div>
+                    
                     </div>';                        
                     }
                     else{
-                    '<div class="col-lg-5 text-left">
+                    '<div class="col-lg-5 text-center">
                     <p>Oled juba hääletanud kandidaat number '.$haal[0]->Valik.' poolt</p>
-                    <div id="kandB"></div>
                     </div>';                          
                     }
                 }
