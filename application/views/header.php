cdee0b1f87644b5f33ce612d42d3e2508cf09f18
Märt Sessman <m2rt.sessman@gmail.com>
2016-04-10 23:03:49 +0300

    .css

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title><?php echo $page_title; ?></title>
 	
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<!--<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" /> -->
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/custom.css"); ?>" />
 
 </head>
