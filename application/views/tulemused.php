3aecb08be35960472644ea1cc53ab75b6c4f5f83
Märt Sessman <m2rt.sessman@gmail.com>
2016-04-10 18:19:08 +0300

    funktsionaalsust tulemused lehele. veidi javascripti

--- application/views/tulemused.php
+++ application/views/tulemused.php
@@ -9,7 +9,7 @@
                     <a class="btn btn-default" id="koik" href="#">Kogu riigis</a>
                     <div class="btn-group" role="group"> 
                         <button id="btnGroupVerticalDrop2" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Vali piirkond<span class="caret"></span> </button>
-                        <ul class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop2">
+                        <ul class="dropdown-menu" id="piirkonnad" aria-labelledby="btnGroupVerticalDrop2">
                             	<?php
 	                           foreach($piirkonnad as $k){
                                        	echo "<li><a href='#piirkond=".$k->Piirkond."'>".$k->Piirkond."</a></li> ";
@@ -19,38 +19,19 @@
                     </div> 
                     <div class="btn-group" role="group"> 
                         <button id="btnGroupVerticalDrop3" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Vali erakond<span class="caret"></span> </button>
-                        <ul class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop3"> 
+                        <ul class="dropdown-menu" id = "erakonnad" aria-labelledby="btnGroupVerticalDrop3"> 
                             	<?php
 	                           foreach($erakonnad as $k){
-                                       	echo "<li id='list'><a href='#erakond=".$k->Erakond."'>".$k->Erakond."</a></li> ";
+                                       	echo "<li><a href='#erakond=".$k->Erakond."'>".$k->Erakond."</a></li> ";
                                     }
                                 ?> 
                         </ul> 
                     </div>
                 </div>
             </div>
-            <div class="col-sm-9 text-left">
+            <div class="col-sm-9 text-left" id="chart">
                 <h3>Tulemused</h3>
-                    <?php
-                        $i = true;
-	                foreach($erakonnad as $k){ 
-                            if($i){
-                                $i = false;
-                                $color = 'success';
-                            }
-                            else{
-                                $i = true;
-                                $color = 'warning';
-                            }
-                                ?>
-                                <div class="progress">
-                                    <div class="progress-bar <?php echo 'progress-bar-'.$color.'" id="'.$k->Erakond?>" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                        <div class="text-left"><?php echo $k->Erakond; ?></div>
-                                    </div>
-                                </div>
-                            <?php
-                        }
-                    ?> 
+
             </div>
             <div class="col-sm-12 text-left">
                 <div class="panel panel-default"> 
@@ -59,8 +40,8 @@
                         <table id="tabel" class="table table-striped tablesorter">
                             <thead> 
                                 <tr>
-                                    <th> <span class="glyphicon glyphicon-sort"></span>Number</th> 
-                                    <th> <span class="glyphicon glyphicon-sort-by-alphabet"></span>Nimi</th> 
+                                    <th><span class="glyphicon glyphicon-sort"></span>Number</th> 
+                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span>Nimi</th> 
                                     <th><span class="glyphicon glyphicon-sort-by-alphabet"></span>Erakond</th> 
                                     <th><span class="glyphicon glyphicon-sort-by-alphabet"></span>Piirkond</th>
                                     <th><span class="glyphicon glyphicon-sort"></span>Hääli</th>
@@ -70,7 +51,7 @@
 					<?php
 	                           	foreach($kandidaadid as $k){
                                         	echo "<tr>";
-                                        	echo "<th scope=\"row\">".$k->Number."</th>";
+                                        	echo "<td scope=\"row\">".$k->Number."</td>";
                                         	echo "<td>".$k->Nimi."</td>";
 	                                        echo "<td>".$k->Erakond."</td>";
         	                                echo "<td>".$k->Piirkond."</td>";
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,11 +4,9 @@
  * and open the template in the editor.
  */
 var date = "2016/05/05";
-function sortTulemused(str){
-    
-}
 // http://hilios.github.io/jQuery.countdown/examples/legacy-style.html
-var hash='';
+var headers = Array();
+var rows = Array();
 $(document).ready(function () {
     $("#countdown").countdown(date, function (event) {
         $(this).html(event.strftime(''
@@ -19,29 +17,21 @@ $(document).ready(function () {
         ));
     });
     $('#tabel').tablesorter();
-    $(function () {
-        var headers = Array();
-        var j = 0;
-        $("table tr th").each(function(i, v){
-            if(i === 0 || i === 2)
-        	headers[j++] = $(this).text();
-        });
-
-        var rows = Array();
-        var j = 0;
-        $("table tr").each(function (i, v) {
-            rows[j] = Array();
-            $(this).children('td').each(function (ii, vv) {
-                if (ii === 0 || ii === 2)
-                    rows[j][ii] = $(this).text();
-            });
-            j++;
-        });
-
-        console.log(headers);
-        console.log(rows);
-    });
     
+    $(function () {
+        headers = $("th", $("#tabel")).map(function () {
+            var $s = this.innerHTML.replace(/<span(.*)>(.*)<\/span>/g, "");
+            return $s;
+        }).get();
+
+        rows = $("tbody tr", $("#tabel")).map(function () {
+            return [$("td", this).map(function () {
+                    return this.innerHTML;
+                }).get()];
+        }).get();
+
+
+    });
     (function ($) {
         $('#otsing').keyup(function () {
 
@@ -55,27 +45,121 @@ $(document).ready(function () {
 
     }(jQuery));
     (function () {
-        $('#list').on('click','li',function () {
-
+        $('#piirkonnad').on('click','li',function () {
             var rex = new RegExp($(this).text(), 'i');
             $('.searchable tr').hide();
             $('.searchable tr').filter(function () {
                 return rex.test($(this).text());
             }).show();
-
+            createErakondChart($(this).text());
 
         });
-
+    }(jQuery));
+        (function () {
+        $('#erakonnad').on('click','li',function () {
+            var rex = new RegExp($(this).text(), 'i');
+            $('.searchable tr').hide();
+            $('.searchable tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+            createLiigeChart($(this).text());
+            
+        });
     }(jQuery));
     (function () {
         $('#koik').click(function () {
 
             var rex = new RegExp($, 'i');
             $('.searchable tr').show();
-
+            createErakondChart("");
         });
-
-    }(jQuery));
-   
+    }(jQuery));   
 
 });
+function createErakondChart($name){
+    var newHeaders = Array();
+    newHeaders[0] = headers[2];
+    newHeaders[1] = headers[4];
+    var newRows = Array();
+    var j = 0;
+    var arrayLength = rows.length;
+    var total_votes = 0;
+    var votes = {};
+    for (var i = 0; i < arrayLength; i++) {
+        if(rows[i][3] === $name || $name === ""){
+            newRows[j++] = rows[i];
+            total_votes += rows[i][4];
+            if (rows[i][2] in votes) {
+                votes[rows[i][2]] += rows[i][4];
+            } else {
+                votes[rows[i][2]] = rows[i][4];
+            }
+        }
+    }
+    //total_votes = 70;
+    var v = 30;
+    if($name === "")
+        $name = "kogu riik";
+    document.getElementById("chart").innerHTML = "<h3>Tulemused</h3>"+
+            "<p>Valitud piirkond: "+$name+"<p>Kokku hääli: "+total_votes+"</p>";
+    for (var key in votes) {
+        if (votes.hasOwnProperty(key)) {
+            //votes[key] = v;
+            //v+=10;
+            $("#chart").append('<div class="progress" style="width:500px;margin-bottom:0px;margin-top:20px;">' +
+                    '<div class="progress-bar" id='+key+' role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 0%;transition-duration: 3s;">' +
+                    '<div class="text-left">'+key+': '+votes[key].toString()+' häält</div>' +
+                    '</div></div>');
+
+            $('#'+key).css("width", votes[key]*100/total_votes+"%");
+            
+            console.log(votes[key]+key);
+            //alert(key + " -> " + votes[key]);
+        }
+    }  
+}
+
+function createLiigeChart($name){
+    var newHeaders = Array();
+    newHeaders[0] = headers[1];
+    newHeaders[1] = headers[4];
+    var newRows = Array();
+    var j = 0;
+    var arrayLength = rows.length;
+    var total_votes = 0;
+    var votes = {};
+    for (var i = 0; i < arrayLength; i++) {
+        if(rows[i][2] === $name || $name === ""){
+            newRows[j++] = rows[i];
+            total_votes += rows[i][4];
+            if (rows[i][1] in votes) {
+                votes[rows[i][1]] += rows[i][4];
+            } else {
+                votes[rows[i][1]] = rows[i][4];
+            }
+        }
+    }
+    //total_votes = 70;
+    var v = 30;
+    if($name === "")
+        $name = "kogu riik";
+    document.getElementById("chart").innerHTML = "<h3>Tulemused</h3>"+
+            "<p>Valitud erakond: "+$name+"<p>Kokku hääli: "+total_votes+"</p>";
+    for (var key in votes) {
+        if (votes.hasOwnProperty(key)) {
+            //votes[key] = v;
+            //v+=10;
+            var id = key.toString().replace(" ", "_");
+            
+            $("#chart").append('<div class="progress" style="width:500px;margin-bottom:0px;margin-top:20px;">' +
+                    '<div class="progress-bar" id='+id+' role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 0%;transition-duration: 3s;">' +
+                    '<div class="text-left">'+key+': '+votes[key]+' häält</div>' +
+                    '</div></div>');
+
+            $('#'+id).css("width", votes[key]*100/total_votes+"%");
+            
+            console.log(votes[key]+" "+key);
+            //alert(key + " -> " + votes[key]);
+        }
+    }  
+}
