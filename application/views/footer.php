2338c0dae4138ceda479ac8ee69df162915a3960
Märt Sessman <m2rt.sessman@gmail.com>
2016-04-14 14:12:01 +0300

    footer impr

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,10 +2,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                <div class="col-lg-6 text-right">
+                <div class="col-sm-6 text-right">
                     <p id="ajutine"><p>
                 </div>
-                <div class="col-lg-6 text-left">
+                <div class="col-sm-6 text-left">
                     <p><?php echo $footer_tekst;?></p>
                 </div>
                 </div>
