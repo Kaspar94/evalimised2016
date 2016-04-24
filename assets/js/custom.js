237a02300a350f4d1c3d5112b452721ae7aa2c81
Märt Sessman <m2rt.sessman@gmail.com>
2016-04-24 13:50:02 +0300

    asd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,14 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var date = document.getElementById("date").innerHTML;
 // http://hilios.github.io/jQuery.countdown/examples/legacy-style.html
 var headers = Array();
 var rows = Array();
 $(document).ready(
     function () { 
     
-    $("#countdown").countdown(date, function (event) {
+    $("#countdown").countdown(document.getElementById("date").textContent, function (event) {
         $(this).html(event.strftime(''
             + '<span class="cd-number">%D</span> päeva '
             + '<span class="cd-number">%H</span> tundi '
