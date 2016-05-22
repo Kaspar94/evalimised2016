f5b72a89be19bdc041bdb0e7f21ac6efe06024fd
Kaspar <valimised16csut@webhost.ut.ee>
2016-05-22 18:19:25 +0300

    lang tulemused

--- application/views/tulemused.php
+++ application/views/tulemused.php
@@ -4,11 +4,11 @@
         <div class="box"> 
             <div class="col-sm-3 text-left">
                 <h3>Tulemused</h3>
-                <p>Kokku hääli: <?php echo $kokku_haali ?></p>
+                <p><?php echo $totalvotes.": ".$kokku_haali ?></p>
                 <div class="btn-group-vertical" role="group" aria-label="Vertical button group" id="list">
-                    <a class="btn btn-default" id="koik" href="#">Kogu riigis</a>
+                    <a class="btn btn-default" id="koik" href="#"><?php echo $wholecountry; ?></a>
                     <div class="btn-group" role="group"> 
-                        <button id="btnGroupVerticalDrop2" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Vali piirkond<span class="caret"></span> </button>
+                        <button id="btnGroupVerticalDrop2" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $chooseregion; ?><span class="caret"></span> </button>
                         <ul class="dropdown-menu" id="piirkonnad" aria-labelledby="btnGroupVerticalDrop2">
                             	<?php
 	                           foreach($piirkonnad as $k){
@@ -18,7 +18,7 @@
                         </ul> 
                     </div> 
                     <div class="btn-group" role="group"> 
-                        <button id="btnGroupVerticalDrop3" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Vali erakond<span class="caret"></span> </button>
+                        <button id="btnGroupVerticalDrop3" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $chooseparty; ?><span class="caret"></span> </button>
                         <ul class="dropdown-menu" id = "erakonnad" aria-labelledby="btnGroupVerticalDrop3"> 
                             	<?php
 	                           foreach($erakonnad as $k){
@@ -40,11 +40,11 @@
                         <table id="tabel" class="table table-striped tablesorter">
                             <thead> 
                                 <tr>
-                                    <th><span class="glyphicon glyphicon-sort"></span>Number</th> 
-                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span>Nimi</th> 
-                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span>Erakond</th> 
-                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span>Piirkond</th>
-                                    <th><span class="glyphicon glyphicon-sort"></span>Hääli</th>
+                                    <th><span class="glyphicon glyphicon-sort"></span><?php echo $number; ?></th> 
+                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span><?php echo $name; ?></th> 
+                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span><?php echo $party; ?></th> 
+                                    <th><span class="glyphicon glyphicon-sort-by-alphabet"></span><?php echo $region; ?></th>
+                                    <th><span class="glyphicon glyphicon-sort"></span><?php echo $votes_lang; ?></th>
                                 </tr> 
                             </thead> 
                             <tbody class="searchable"> 
