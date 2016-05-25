95be7c33d7e560cd84e65219c6dd8aacd5a6a0bf
Märt Sessman <m2rt.sessman@gmail.com>
2016-05-25 23:09:11 +0300

    kandideeri error message tõlge

--- application/controllers/Sait.php
+++ application/controllers/Sait.php
@@ -305,7 +305,7 @@ class Sait extends CI_Controller {
             }
             $this->form_validation->set_rules('piirkond', 'Piirkond', 'callback_combo_check');
             $this->form_validation->set_rules('erakond', 'Erakond', 'callback_combo_check');
-            $this->form_validation->set_rules('slogan', 'lang:slogan', 'required|max_length[64]|callback_sloganerror['.$slogan_error.']');
+            $this->form_validation->set_rules('loosung', 'lang:slogan', 'required|max_length[64]|callback_sloganerror['.$slogan_error.']');
             if ($this->form_validation->run() == FALSE) {
                 //fail validation
                 $this->load->view('kandideeri', $this->data);
